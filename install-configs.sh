72b079a7f84ac38efe3107f2eccc5735c124a0cd
Renan Lordello <renan.lordello@bytedance.com>
2025-07-08 16:01:36 -0300

    Creates config installation script

---
+++ install-configs.sh
@@ -0,0 +1,7 @@
+#! /bin/zsh
+ln -s $HOME/dotfiles/asdf/tool-versions $HOME/.tool-versions
+ln -s $HOME/dotfiles/ghostty $HOME/.config/ghostty
+ln -s $HOME/dotfiles/git $HOME/.config/git
+ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/tmux $HOME/.config/tmux
+ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
